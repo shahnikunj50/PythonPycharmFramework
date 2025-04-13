59efefefcc5bceb5414b7499c8777bc15753d71d
Nikunj Shah <shahnikunj50@gmail.com>
2025-04-14 00:26:04 +0530

    Delete tests directory

--- tests/test_login.py
+++
@@ -1,9 +0,0 @@
-import config
-from pages.login_page import LoginPage
-import time
-
-def test_login_success(driver):
-    login_page = LoginPage(driver)
-    login_page.load(config.BASE_URL)
-    login_page.login(config.USERNAME,config.PASSWORD)
-    assert "index" in driver.current_url.lower()
